1217cb1ef3d8a6ada9f0173d9a6a7552674cdfad
gvantsa gvagvalia <gvantsa.gvagvalia1@gmail.com>
2024-11-16 15:23:08 +0400

    added Product Filter By Category.

---
+++ Trendzy/Views/ProfileView/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Trendzy
+//
+//  Created by gvantsa gvagvalia on 11/15/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
